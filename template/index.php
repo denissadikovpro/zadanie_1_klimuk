e2d8f340009b3eaaec1dbbad093d49adf1efab43
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-03-07 19:03:34 +0200

    Vytvaranie funkcii zavolaj_footer a zavolaj_header cez PHP

--- template/index.php
+++ template/index.php
@@ -46,7 +46,42 @@ https://templatemo.com/tm-564-plot-listing
   <!-- ***** Preloader End ***** -->
 
   <!-- ***** Header Area Start ***** -->
+ <!-- ***** Header Area Start ***** -->
+<?php
+function zavolaj_header() {
+?>
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <a href="index.html" class="logo">
+                    </a>
+                    <ul class="nav">
+                        <li><a href="index.html" class="active">Home</a></li>
+                        <li><a href="category.html">Category</a></li>
+                        <li><a href="listing.html">Listing</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                        <li><div class="main-white-button"><a href="#"><i class="fa fa-plus"></i> Add Your Listing</a></div></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<?php
+}
+?>
 
+<!-- Volame funkciu -->
+
+<?php
+include_once 'header.php';
+zavolaj_header();
+?>
   <!-- ***** Header Area End ***** -->
 
   <div class="main-banner">
@@ -551,7 +586,77 @@ https://templatemo.com/tm-564-plot-listing
   </div>
 
   <!-- ***** Footer Area Start ***** -->
+   <!-- Vytvorime funkciu -->
+  <?php
+function zavolaj_footer() {
+?>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="about">
+                    <div class="logo">
+                        <img src="assets/images/black-logo.png" alt="Plot Listing">
+                    </div>
+                    <p>If you consider that <a rel="nofollow" href="https://templatemo.com/tm-564-plot-listing" target="_parent">Plot Listing template</a> is useful for your website, please <a rel="nofollow" href="https://www.paypal.me/templatemo" target="_blank">support us</a> a little via PayPal.</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="helpful-links">
+                    <h4>Helpful Links</h4>
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-6">
+                            <ul>
+                                <li><a href="#">Categories</a></li>
+                                <li><a href="#">Reviews</a></li>
+                                <li><a href="#">Listing</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Awards</a></li>
+                                <li><a href="#">Useful Sites</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="contact-us">
+                    <h4>Contact Us</h4>
+                    <p>27th Street of New Town, Digital Villa</p>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="#">010-020-0340</a>
+                        </div>
+                        <div class="col-lg-6">
+                            <a href="#">090-080-0760</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="sub-footer">
+                    <p>Copyright © 2021 Plot Listing Co., Ltd. All Rights Reserved.
+                    <br>
+                    Design: <a rel="nofollow" href="https://templatemo.com" title="CSS Templates">TemplateMo</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php
+}
+?>
 
+<!-- A zavolame funkciu -->
+<?php
+include_once 'footer.php';
+zavolaj_footer();
+?>
   <!-- ***** Footer Area End ***** -->
 
 
